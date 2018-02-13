93ca09fe0b518831ccd075f1bb6726d8c306584a
Zhang Matt <mzhang@lcs.on.ca>
2018-02-12 21:45:36 -0500

    i did it exclamation mark

--- Collection Types.playground/Pages/Exercise One.xcplaygroundpage/Contents.swift
+++ Collection Types.playground/Pages/Exercise One.xcplaygroundpage/Contents.swift
@@ -9,22 +9,37 @@
 
 import Foundation
 
-
+var grades : [Double] = []
 /*:
  2. Now populate the array with eight random grades between 60 and 100.
  */
-
-
+for _ in 1...8
+{
+    grades.append(Double (arc4random_uniform(40)+60))
+}
+print("\(grades.count)")
 /*:
  3. Print a list of each array index and the associated value.
  */
-
+for (index, value) in grades.enumerated() {
+    print("At \(index) the value is \(value)")
+}
 
 /*:
  4. Determine and print the highest grade.
  */
 
+var highestGrade : Double = 61
 
+for grade in grades
+{
+    if grade > highestGrade
+    {
+        highestGrade = grade
+    }
+}
+
+print("Your highest grade is \(highestGrade)")
 //: [Next](@next)
 
 
--- Collection Types.playground/Pages/Exercise Two.xcplaygroundpage/Contents.swift
+++ Collection Types.playground/Pages/Exercise Two.xcplaygroundpage/Contents.swift
@@ -12,23 +12,67 @@
 import Foundation
 
 
+var restaurantRatings : [Double] = []
 /*:
  2. Now populate the array with twenty random ratings between 1 and 100.
  */
-
-
+for _ in 1...20
+{
+    restaurantRatings.append(Double (arc4random_uniform(100)))
+}
+print("\(restaurantRatings)")
 /*:
  3. Determine the average rating. Remember, calculate an average by finding the sum of all the values, then dividing by the number of values.
  */
-
+var addedRatings : Double = 0
+for ratings in restaurantRatings
+{
+    addedRatings += ratings
+}
+addedRatings/Double(restaurantRatings.count)
 
 /*:
  4. The restaurant is most concerned with how many diners considered their meal to be less than satisfactory. Calculate the percentage of diners who gave the restaurant a rating less than 80.
  */
-
+var unsatisfiedDiners = 0
+for ratings in restaurantRatings
+{
+    if ratings < 80
+    {
+        unsatisfiedDiners += 1
+    }
+}
+var percentage = Double(addedRatings)/Double(restaurantRatings.count)
 /*:
  5. The manager wants more detail. Prepare a report that indicates how many diners found the restuarant "Very Bad" (0 to 25), "Bad" (25 to 49), "Fair" (50 to 79), "Satisfactory" (80 to 89), and "Stellar" (90 to 100).
  */
+var veryBad = 0
+var bad = 0
+var fair = 0
+var satisfactory = 0
+var stellar = 0
 
-
+for ratings in restaurantRatings
+{
+    if ratings < 26
+    {
+        veryBad += 1
+    }
+    else if ratings < 50
+    {
+        bad += 1
+    }
+    else if ratings < 80
+    {
+        fair += 1
+    }
+    else if ratings < 90
+    {
+        satisfactory += 1
+    }
+    else
+    {
+        stellar += 1
+    }
+}
 
